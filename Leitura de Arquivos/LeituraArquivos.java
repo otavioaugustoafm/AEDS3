1a8bbc37360048c8524d7ba0289a907175eaccbb
otavioaugustoafm <otavioaugustoafm@gmail.com>
2025-02-18 16:19:03 -0300

    Leitura de Arquivos

--- Leitura de Arquivos/LeituraArquivos.java
+++ Leitura de Arquivos/LeituraArquivos.java
@@ -4,24 +4,27 @@ import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 
-public class FluxoEscrita {
+public class LeituraArquivos {
     public static void main(String[] args) {
         Livro l1 = new Livro(1, "Eu, Robô","Isaac Asimov", 14.9F);
         Livro l2 = new Livro(2, "Eu Sou A Lenda","Richard Matherson", 21.99F);
         Livro l3 = new Livro(3, "Teste Título", "Teste Autor", 50.9F);
 
+        FileOutputStream arq;
+        DataOutputStream dos;
+
+        System.out.println("Cópia dos livros que foram ESCRITOS no arquivo: ");
         System.out.println(l1);
         System.out.println(l2);
         System.out.println(l3);
 
-        FileOutputStream arq;
-        DataOutputStream dos;
-
         FileInputStream arq2;
         DataInputStream dis;
-    
 
         try {
+
+            /* Escrita */
+
             arq = new FileOutputStream("dados/livros.db");
             dos = new DataOutputStream(arq);
 
@@ -42,7 +45,29 @@ public class FluxoEscrita {
 
             dos.close();
             arq.close();
-            
+
+            /* Leitura */
+
+            Livro l4 = new Livro();
+            Livro l5 = new Livro();
+
+            arq2 = new FileInputStream("dados/livros.db");
+            dis = new DataInputStream(arq2);
+
+            l4.idLivro = dis.readInt();
+            l4.titulo = dis.readUTF();
+            l4.autor = dis.readUTF();
+            l4.preco = dis.readFloat();
+
+            l5.idLivro = dis.readInt();
+            l5.titulo = dis.readUTF();
+            l5.autor = dis.readUTF();
+            l5.preco = dis.readFloat();
+
+            System.out.println("\nLivros que foram LIDOS do arquivo: ");
+            System.out.println(l4);
+            System.out.println(l5);
+
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -62,6 +87,13 @@ class Livro {
         this.titulo = titulo;
         this.autor = autor;
         this.preco = preco;
+    }
+
+    public Livro() {
+        this.idLivro = -1;
+        this.titulo = "";
+        this.autor = "";
+        this.preco = 0F;
     }
 
     public String toString() {
